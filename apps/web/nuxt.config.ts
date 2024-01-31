37d84ab28e131166c8b46961affcda618ea581be
ridhozhr10 <azharridho42@gmail.com>
2024-02-01 05:56:18 +0700

    init project

---
+++ apps/web/nuxt.config.ts
@@ -0,0 +1,40 @@
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { defineNuxtConfig } from 'nuxt/config';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  workspaceDir: '../../',
+  srcDir: 'src',
+  buildDir: '../../dist/apps/web/.nuxt',
+  devtools: { enabled: true },
+  devServer: {
+    host: 'localhost',
+    port: 4200,
+  },
+  typescript: {
+    typeCheck: true,
+    tsConfig: {
+      extends: './tsconfig.app.json',
+    },
+  },
+  imports: {
+    autoImport: false,
+  },
+
+  css: ['~/assets/css/main.css', '~/assets/css/styles.css'],
+
+  vite: {
+    plugins: [nxViteTsPaths()],
+  },
+  nitro: {
+    output: {
+      dir: '../../dist/apps/web/.output',
+    },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
---
+++ apps/web/src/pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <NxWelcome title="jelek mnyet"/>
+</template>
+
+<style>
+@media (min-width: 768px) {
+  .about {
+    max-width: 768px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 0 1rem;
+  }
+}
+</style>
